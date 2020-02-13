c513b499242d08fca7a46d24056d84935ff2b639
Mitchell Kossoris <mkossoris@gmail.com>
2020-02-13 02:10:33 -0800

    connect to api

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const EventSource = require('eventsource');
+const request = require('request');
 
-var source = new EventSource('http://stream.pushshift.io');
+const apiKey = process.env.API_KEY;
+const baseApiUrl = process.env.API_URL;
+
+const source = new EventSource('http://stream.pushshift.io');
 const regex = /^(\!RedditDiamond)/
 
 source.addEventListener('rc', (e) => {
-  var comment = JSON.parse(e.data);
+  const comment = JSON.parse(e.data);
 
   if (comment.body.match(regex)) {
-    console.log(comment);
+    postComment(comment);
   }
 })
+
+function postComment(comment) {
+  const requestOptions = {
+    headers: {
+      'x-api-key': apiKey,
+      'Content-Type': 'application/json'
+    },
+    json: comment
+  };
+
+  request.post(`${baseApiUrl}/test`, requestOptions, (error, response, body) => {
+    if (error) {
+      console.log("Error:", error)
+    } else {
+      console.log("Response:", body);
+    }
+  })
+}
