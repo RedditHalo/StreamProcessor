81d9be7ea39ba04ca31945c1600561ecef1335c2
Scooper <dssiy@uwm.edu>
2020-05-30 19:50:37 -0500

    Processing inbox rather than all comments.

--- index.js
+++ index.js
@@ -1,42 +1,62 @@
-const EventSource = require('eventsource');
 const request = require('request');
+const snoowrap = require('snoowrap');
+const InboxStream = require('snoostorm').InboxStream;
 
-const apiKey = process.env.API_KEY;
-const baseApiUrl = process.env.API_URL;
+const client =  new snoowrap({
+  userAgent: process.env.USER_AGENT,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD
+});
 
-const source = new EventSource('https://stream.pushshift.io/?type=comments&filter=id,body');
-const addHaloRegex = /^(\!RedditHalo)/
-var countt = 0;
+const inbox = new InboxStream(client);
+inbox.on("item", handleMessage);
 
-source.addEventListener('rc', (e) => {
-  const comment = JSON.parse(e.data);
+async function handleMessage(message){
+  console.log(`Processing message with id: ${message.id}`);
 
-  countt++;
-  if (countt % 500 === 0) console.log(comment.id)
- 
-  if (comment.body.match(addHaloRegex)) {
-    addHalo(comment);
-  }
-})
+  if (message.constructor.name === "Comment") {
+    const comment = await message.fetch();
+
+    if (comment.saved) {
+      return;
+    }
+
+    if (comment.body.match(/u\/DonateOrElse( add)?/gi)) {
+      addHalo(comment);
+    }
+
+    message.save();
+  } else if (message.constructor.name === "PrivateMessage") {
+    const pm = await message.fetch();
+
+    //TODO process then mark as read
+    
+    await pm.markAsRead();
+  } 
+}
 
 function addHalo(comment) {
-  console.log(`Adding halo from comment ${comment.id}`)
+  console.log(`Adding halo from comment ${comment.id}`);
+
   const requestOptions = {
     headers: {
-      'x-api-key': apiKey,
+      'x-api-key': process.env.HALO_API_KEY,
       'Content-Type': 'application/json'
     },
     json: {
       commentId: comment.id
     }
   };
-  console.log("requestOptions", requestOptions)
 
-  request.post(`${baseApiUrl}/add-halo`, requestOptions, (error, response, body) => {
+  console.log("requestOptions", requestOptions);
+
+  request.post(`${process.env.HALO_API_ENDPOINT}/add-halo`, requestOptions, (error, response, body) => {
     if (error) {
-      console.log("Error:", error)
+      console.log("Error:", error);
     } else {
       console.log("Response:", body);
     }
-  })
+  });
 }
